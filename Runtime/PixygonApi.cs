967194fd95fbadbf9c70a242c066bbbb755f867a
gentlemanSoup <anders.horne@gmail.com>
2023-08-23 13:57:48 +0200

    Update PixygonApi.cs

--- Runtime/PixygonApi.cs
+++ Runtime/PixygonApi.cs
@@ -218,9 +218,9 @@ namespace Pixygon.Passport {
             await GetWWW("client/feedbacks");
         }
         public async Task<string> GetHighScores(string gameId, string scoretype, DateTime fromDate, DateTime toDate) {
-            var www = await GetWWW($"client/highscores/{gameId}/{scoretype}"); ///{fromDate}/{toDate}");
+            var www = await GetWWW($"client/highscores/{gameId}/{scoretype}/{fromDate}/{toDate}");
             Debug.Log($"highScore: {www.downloadHandler.text}");
-            return www.downloadHandler.text;
+            return "{ highscores: " + www.downloadHandler.text + "}";
         }
         public async void PostHighScore(string game, string user, int score, int kills, float time, string detail) {
             var www = await PostWWW("client/highscores", JsonUtility.ToJson(new HighScore(game, user, score, kills, time, detail)));
