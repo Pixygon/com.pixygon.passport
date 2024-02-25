b69deb634e2d083b852116a4c8913f88750fc30e
gentlemanSoup <anders.horne@gmail.com>
2024-02-25 15:12:12 +0100

    asfasf

--- Runtime/AccountScreens/RegisterPanel.cs
+++ Runtime/AccountScreens/RegisterPanel.cs
@@ -15,8 +15,7 @@ namespace Pixygon.Passport {
         [SerializeField] private GameObject _usernameVerified;
         [SerializeField] private GameObject _emailVerified;
         [SerializeField] private GameObject _passwordVerified;
-        //[SerializeField] private TextMeshProUGUI _passwordErrorText;
-        [SerializeField] private GameObject _passwordError;
+        [SerializeField] private TextMeshProUGUI _passwordErrorText;
         
         private bool _isInputVerified;
         
@@ -27,12 +26,11 @@ namespace Pixygon.Passport {
         }
         public void VerifyInput() {
             _isInputVerified = true;
-            //_passwordErrorText.text =
-            //    "Password must be 8 characters long, contain a number and both upper and lowercase letters.";
+            _passwordErrorText.text =
+                "Password must be 8 characters long, contain a number and both upper and lowercase letters.";
             _usernameVerified.SetActive(false);
             _emailVerified.SetActive(false);
             _passwordVerified.SetActive(false);
-            _passwordError.SetActive(false);
 
             if (_signUpUserInput.text.Length < 1) _isInputVerified = false;
             else _usernameVerified.SetActive(true);
@@ -41,17 +39,16 @@ namespace Pixygon.Passport {
             else _emailVerified.SetActive(true);
 
             if (_signUpPassInput.text == "") {
-                //_passwordErrorText.text =
-                //    "Password must be 8 characters long, contain a number and both upper and lowercase letters.";
+                _passwordErrorText.text =
+                    "Password must be 8 characters long, contain a number and both upper and lowercase letters.";
                 _isInputVerified = false;
             } else {
                 if (_signUpPassInput.text.Length < 8) {
-                    //_passwordErrorText.text = "<color=#FF958B>Password must be longer than 8 digits!";
+                    _passwordErrorText.text = "<color=#FF958B>Password must be longer than 8 digits!";
                     _isInputVerified = false;
                 }
                 if (!_signUpPassInput.text.Any(c => char.IsDigit(c))) {
-                    _passwordError.SetActive(true);
-                    //_passwordErrorText.text = "<color=#FF958B>Password must contain at least one number!";
+                    _passwordErrorText.text = "<color=#FF958B>Password must contain at least one number!";
                     _isInputVerified = false;
                 } else {
                     _passwordVerified.SetActive(true);
@@ -62,8 +59,7 @@ namespace Pixygon.Passport {
 
             Debug.Log("Input verified: " + _isInputVerified);
             if (!_isInputVerified) return;
-            //_passwordErrorText.text =
-            //    "Password must be 8 characters long, contain a number and both upper and lowercase letters.";
+            _passwordErrorText.text = "";
             _isInputVerified = true;
         }
         public void Signup() {
